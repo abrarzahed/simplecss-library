f7684f73e147e274364ae17d8faf2216033803d0
abrarzahed <abrarzahed6986@gmail.com>
2022-10-12 13:34:59 +0600

    completed

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
 
 function buildStyles() {
-  return src("simplecss/**/*.scss").pipe(sass()).pipe(dest("css"));
+  return src("simplecss/**/*.scss")
+    .pipe(sass())
+    .pipe(
+      purgecss({
+        content: ["*.html"],
+      })
+    )
+    .pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(["simplecss/**/*.scss"], buildStyles);
+  watch(["simplecss/**/*.scss", "*.html"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
